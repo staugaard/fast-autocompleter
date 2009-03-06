b970b3b2a49a9509e0671005b436f994963cdab6
Mick Staugaard <spam@staugaard.com>
2009-03-06 14:56:16 +0100

    wiki update

--- wiki_examples/single_value_example.js
+++ wiki_examples/single_value_example.js
@@ -1,1 +1,11 @@
+function lookup(searchString, callback) {
+  new Ajax.Request('/users/autocomplete', { parameters: {name: searchString, rand: (new Date()).getTime()},
+                                            onSuccess: function(response) {
+                                              callback(response.responseJSON);
+                                            } });
+}
+
+var cachedBackend = new Autocompleter.Cache(lookup, {choices: 10});
+var cachedLookup = cachedBackend.lookup.bind(cachedBackend);
+
 new Autocompleter.Json('autocomplete', 'autocomplete_choices', lookup, {frequency: 0.1});
